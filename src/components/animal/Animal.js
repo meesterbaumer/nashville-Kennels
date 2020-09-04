7468f5268d76f07569af4a81873a5fc27c40d7ea
Michael Baumer <meeesterbaumer@gmail.com>
2020-09-04 10:59:06 -0500

    got correct properties showing in animal list

--- src/components/animal/Animal.js
+++ src/components/animal/Animal.js
@@ -1,9 +1,11 @@
 import React from "react"
 import "./Animal.css"
 
-export const Animal = ({animal}) => (
+export const Animal = ({animal, location, customer}) => (
     <section className="animal">
         <h3 className="animal__name">{animal.name}</h3>
-        <div className="animal__breed">{animal.breed}</div>
+        <div className="animal__breed">breed: {animal.breed}</div>
+        <div className="animal__location">Location: {location.name}</div>
+        <div className="animal__customer">Customer: {customer.name}</div>
     </section>
 )
--- src/components/animal/AnimalList.js
+++ src/components/animal/AnimalList.js
@@ -2,10 +2,14 @@ import React, { useContext, useEffect } from "react"
 import { AnimalContext } from "./AnimalProvider";
 import { Animal } from "./Animal";
 import "./Animal.css"
+import { LocationContext } from "../location/LocationProvider";
+import { CustomerContext } from "../customer/CustomerProvider";
 
 export const AnimalList = () => {
   // This state changes when `getAnimals()` is invoked below
   const { animals, getAnimals } = useContext(AnimalContext)
+  const { locations, getLocations } = useContext(LocationContext)
+  const { customers, getCustomers } = useContext(CustomerContext)
 
   /*
         What's the effect this is reponding to? Component was
@@ -13,13 +17,21 @@ export const AnimalList = () => {
         then gets the data, then re-renders.
     */
   useEffect(() => {
-    getAnimals()
+    getAnimals().then(getLocations).then(getCustomers)
   }, [])
 
   return (
     <article className="animals">
       {
-        animals.map(animal => <Animal key={animal.id} animal={animal} />)
+        animals.map(animal => {
+          const owner = customers.find(c => c.id === animal.customerId) || {}
+          const clinic = locations.find(l => l.id === animal.locationId) || {}
+
+          return <Animal key={animal.id}
+          location={clinic}
+          customer={owner}
+          animal={animal} />
+        })
       }
     </article>
   )
