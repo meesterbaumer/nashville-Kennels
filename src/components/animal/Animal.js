674d9caa82619653616b8eedfed33027a0e6ee03
Michael Baumer <meeesterbaumer@gmail.com>
2020-09-10 14:15:30 -0500

    WIP

--- src/components/animal/Animal.js
+++ src/components/animal/Animal.js
@@ -1,11 +1,14 @@
 import React from "react"
 import "./Animal.css"
+import { Link } from "react-router-dom"
 
-export const Animal = ({animal, location, customer}) => (
+export default ({ animal }) => (
     <section className="animal">
-        <h3 className="animal__name">{animal.name}</h3>
-        <div className="animal__breed">breed: {animal.breed}</div>
-        <div className="animal__location">Location: {location.name}</div>
-        <div className="animal__customer">Customer: {customer.name}</div>
+        <h3 className="animal__name">
+            <Link to={`/animals/${animal.id}`}>
+                { animal.name }
+            </Link>
+        </h3>
+        <div className="animal__breed">{ animal.breed }</div>
     </section>
 )
